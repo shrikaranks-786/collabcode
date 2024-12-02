7c8adebf7c16d6e86d5619993f777accfc6def1c
shrikaran ks <shrikaranksise2024@gmail.com>
2024-12-02 14:49:58 +0530

    finished desingning the homepage

--- src/pages/Editorpage.js
+++ src/pages/Editorpage.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 function Editorpage() {
-  return (
-    <div>Editorpage</div>
-  )
+    return (
+        <div className="homepagewrapper">
+            <div className="formwrapper">
+                <img className='homepagelogo' src='/collab-code.png' alt='collab-code logo'/>
+                <h4 className='mainLabel'>Paste Your Invitaion room id here</h4>
+                <div className='inputgroup'>
+                    <input type='text' className='inputbox' placeholder='ROOM-ID'/>
+                    <input type='text' className='inputbox' placeholder='USER-NAME'/>
+                    <button className='btn joinbtn'>Join</button>
+                    <span className='createinfo'>
+                        If you dont have an invite then create &nbsp;
+                        <a href='' className='createnewbtn'>new room</a>
+                    </span>
+                </div>
+            </div>
+            <footer>
+                <h4>Built by <a href='https://github.com/shrikaranks-786/collabcode'>Shrikaran</a></h4>
+            </footer>
+        </div>
+    );
 }
 
-export default Editorpage
+export default Editorpage;
